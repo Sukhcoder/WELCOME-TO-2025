d40db105a14e531aee085966e84f7ae964f3b2c4
Sukhcoder <happymindj56@gmail.com>
2025-01-01 10:34:48 +0000

    Update script.js

--- script.js
+++ script.js
@@ -23,6 +23,6 @@ var x = setInterval(function() {
   // If the count down is finished, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("countdown").innerHTML = "HAPPY NEW YEAR!";
+    document.getElementById("countdown").innerHTML = "Welcome to 2025!";
   }
 }, 1000);
